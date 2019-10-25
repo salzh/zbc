c8e0306ff5ca2035e9eac36b18db852e6d828f47
salzh <zhongxiang721@gmail.com>
2019-10-25 11:42:28 +0800

    update

--- newfies/survey/forms.py
+++ newfies/survey/forms.py
@@ -630,6 +630,33 @@ class BranchingForm(ModelForm):
                     css_class='row'
                 )
             )
+        elif obj_section.type == SECTION_TYPE.AVMD:
+            self.helper.layout.append(
+                Div(
+                    Div(HTML(
+                        """
+                        <div class="btn-group" data-toggle="buttons">
+                            <label class="btn btn-default">
+                                <input type="radio" name="keys_button" id="button-human"> %s
+                            </label>
+                            <label class="btn btn-default">
+                                <input type="radio" name="keys_button" id="button-machine"> %s
+                            </label>
+                            <label class="btn btn-default">
+                                <input type="radio" name="keys_button" id="button-unsure"> %s
+                            </label>
+                        </div>
+                        """ % ('HUMAN', 'MACHINE','unsure')
+                    ), css_class=css_class),
+                    css_class='row'
+                )
+            )
+            self.helper.layout.append(
+                Div(
+                    Div(Fieldset('', 'keys', 'goto'), css_class=css_class),
+                    css_class='row'
+                )
+            )
         else:
             if obj_section.type != SECTION_TYPE.HANGUP_SECTION and obj_section.type != SECTION_TYPE.DNC:
                 self.helper.layout.append(
